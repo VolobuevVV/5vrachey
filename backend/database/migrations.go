452523864e01a42b78452448cd537f25c5abd598
Vladimir <vladimir.volobuev2k02@mail.ru>
2025-09-23 22:13:22 +0100

    Добавлено получение данных из БД

---
+++ backend/database/migrations.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"log"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func RunMigrations() {
+	driver, err := postgres.WithInstance(DB, &postgres.Config{})
+	if err != nil {
+		log.Fatal("Error creating migration driver:", err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://migrations",
+		"postgres", driver)
+	if err != nil {
+		log.Fatal("Error creating migration instance:", err)
+	}
+
+	err = m.Up()
+	if err != nil && err != migrate.ErrNoChange {
+		log.Fatal("Error applying migrations:", err)
+	}
+
+	log.Println("Migrations applied successfully!")
+}
--- backend/main.go
+++ backend/main.go
@@ -1,90 +1,27 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"log"
+	"my-app/database"
 	"my-app/handlers"
 	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database/postgres"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
 )
 
-var DB *sql.DB
-
-func InitDB() {
-	connStr := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_SSLMODE"),
-	)
-
-	var err error
-	DB, err = sql.Open("postgres", connStr)
-	if err != nil {
-		log.Fatal("Error connecting to database:", err)
-	}
-
-	err = DB.Ping()
-	if err != nil {
-		log.Fatal("Error pinging database:", err)
-	}
-
-	log.Println("Connected to database successfully!")
-}
-
-func runMigrations() {
-	driver, err := postgres.WithInstance(DB, &postgres.Config{})
-	if err != nil {
-		log.Fatal("Error creating migration driver:", err)
-	}
-
-	migrationsPath := "file://migrations"
-	if _, err := os.Stat("/app/backend/migrations"); err == nil {
-		migrationsPath = "file:///app/backend/migrations" // в контейнере
-	}
-
-	m, err := migrate.NewWithDatabaseInstance(
-		migrationsPath,
-		"postgres", driver)
-	if err != nil {
-		log.Fatal("Error creating migration instance:", err)
-	}
-
-	err = m.Up()
-	if err != nil && err != migrate.ErrNoChange {
-		log.Fatal("Error applying migrations:", err)
-	}
-
-	log.Println("Migrations applied successfully!")
-}
-
 func main() {
-	files, _ := os.ReadDir("/app/backend/migrations")
-	log.Printf("Files in /app/backend/migrations:")
-	for _, file := range files {
-		log.Printf(" - %s", file.Name())
-	}
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
 
-	InitDB()
-	runMigrations()
+	database.InitDB()
+	database.RunMigrations()
 
 	router := gin.Default()
 
-	promotionHandler := &handlers.PromotionHandler{DB: DB}
+	promotionHandler := &handlers.PromotionHandler{DB: database.DB}
 	router.GET("/api/top-panel-text", promotionHandler.GetPromotionText)
 
 	port := os.Getenv("SERVER_PORT")
